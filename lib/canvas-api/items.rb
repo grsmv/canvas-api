7942953d125b170082fef9a99861af850791a03a
Sergey Gerasimov <mail@grsmv.com>
2015-07-08 14:52:23 +0300

    study plan: getting remote items

--- lib/canvas-api/items.rb
+++ lib/canvas-api/items.rb
@@ -2,7 +2,9 @@ module Canvas
   class API
     def items(course_id:, module_id:)
       endpoint = construct_endpoint __method__, ids: { course_id: course_id, module_id: module_id }
-      HTTParty.get(endpoint).map &:to_struct
+      HTTParty.get(endpoint).map do |item|
+        item.merge({payload: {}}).to_struct
+      end
     end
   end
 end
--- lib/canvas-api/study_plan.rb
+++ lib/canvas-api/study_plan.rb
@@ -4,8 +4,19 @@ module Canvas
   class API
     def study_plan(course_id:)
       course_modules = modules(course_id: course_id)
+
+      update_items_with_remote_data = -> (items) do
+        Parallel.map(items, in_threads: items.size) do |item|
+          if %w(Assignment Quiz Discussion).include? item.type
+            item.payload = self.send(item.type.downcase, course_id: course_id, content_id: item.content_id)
+          end
+          item
+        end
+      end
+
       Parallel.map(course_modules, in_threads: course_modules.size) do |modul|
-        modul.tap { |m| m.items = items(course_id: course_id, module_id: modul.id) }
+        modul.items = update_items_with_remote_data.call items(course_id: course_id, module_id: modul.id)
+        modul
       end
     end
   end
