24c6c5a3966a09d5f248060504da75a392aa2910
AndriySolonyna <asolon@softserveinc.com>
2015-07-29 14:56:02 +0300

    fix submissions api

--- lib/canvas-api/submissions.rb
+++ lib/canvas-api/submissions.rb
@@ -2,7 +2,7 @@ module Canvas
   class API
 
     def submissions(section_id:, assignment_id:)
-      get_collection __method__, ids: { section_id: course_id, assignment_id: assignment_id }
+      get_collection __method__, ids: { section_id: section_id, assignment_id: assignment_id }
     end
 
   end
