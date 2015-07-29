0a73ac439790763d2f6cef9501649e6085a08798
AndriySolonyna <asolon@softserveinc.com>
2015-07-29 17:18:47 +0300

    add example

--- lib/canvas-api/conversations.rb
+++ lib/canvas-api/conversations.rb
@@ -1,6 +1,16 @@
 module Canvas
   class API
 
+
+    # Usage:
+    #   ap c.create_conversation(
+                                  # body: {
+                                          # recipients: [3336],
+                                          # subject:    'Subject',
+                                          # body:       'Text ...'
+
+                                  # })
+
     def create_conversation(body: {})
       post_single __method__, body: body
     end
