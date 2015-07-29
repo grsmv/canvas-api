fe54ccfd811d690e4c9465637498408e776dfd2e
AndriySolonyna <asolon@softserveinc.com>
2015-07-29 17:01:10 +0300

    fix conversations

--- lib/canvas-api.rb
+++ lib/canvas-api.rb
@@ -15,7 +15,7 @@ require_relative './canvas-api/section_enrollments'
 require_relative './canvas-api/study_plan'
 require_relative './canvas-api/quiz'
 require_relative './canvas-api/submissions'
-require_relative '.canvas-api/conversations'
+require_relative './canvas-api/conversations'
 require_relative './canvas-api/version'
 
 class Object
@@ -42,7 +42,7 @@ module Canvas
     update_assignment_override: '/api/v1/courses/%{course_id}/assignments/%{assignment_id}/overrides/%{override_id}',
     delete_assignment_override: '/api/v1/courses/%{course_id}/assignments/%{assignment_id}/overrides/%{override_id}',
     submissions:                '/api/v1/sections/%{section_id}/assignments/%{assignment_id}/submissions',
-    create_conversation:       '/api/v1/conversations'
+    create_conversation:        '/api/v1/conversations'
   }
 
   # Main class. All useful work we are doing here. Should be initialised using
