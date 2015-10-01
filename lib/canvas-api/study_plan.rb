6a9e776ada7edd0b82c0e0c224a173cccd582d76
Kacey Roberts <kroberts@instructure.com>
2015-10-01 15:38:49 -0600

    Fix needed for flexable learning

--- lib/canvas-api/study_plan.rb
+++ lib/canvas-api/study_plan.rb
@@ -16,10 +16,38 @@ module Canvas
     #
     def study_plan(course_id:)
       course_modules = modules(course_id: course_id)
+
+      # Updating IDs for discussions and quizzes 'cause Canvas has 2 IDs for
+      # each corresponding item - all depends on a way which we use to retrieve them.
+      # And we should use one, which came from `assignments` endpoint.
+      assignments = self.assignments(course_id: course_id)
+      update_ids_for_discussions_and_quizzes = -> (items) do
+        items.map do |item|
+          if %w(Quiz Discussion).include? item.type
+            filtrated = assignments.select do |a|
+              item.content_id == case item.type
+                                   when 'Discussion'
+                                     a.discussion_topic['id'] if a.respond_to? :discussion_topic
+                                   when 'Quiz'
+                                     a.quiz_id if a.respond_to? :quiz_id
+                                 end
+            end
+            if filtrated.size.zero? || filtrated[0].nil?
+              item = nil
+            else
+              item.id = filtrated.first.id
+            end
+          end
+          item
+        end
+      end
+
       update_module = lambda do |m|
         m.tap do |mod|
           unless mod.items_count.zero?
-            items = self.items(course_id: course_id, module_id: mod.id)
+            items = update_ids_for_discussions_and_quizzes.call(self.items course_id: course_id, module_id: mod.id)
+            #filter out stuff that doesn't have a content_id, this means it's not an assignment
+            items = items.select{|item| (item.respond_to?('content_id'))}
             mod.items = fill_due_dates course_id, items
           end
         end
@@ -29,20 +57,24 @@ module Canvas
         course_modules.map(&update_module) :
         Parallel.map(course_modules, in_threads: course_modules.size, &update_module)
     end
-    
+
     private
 
     def fill_due_dates(course_id, items)
       update_item = lambda do |item|
+
         item.tap do |item|
           if self.class.assignment?(item)
-            assignment_details = self.send(item.type.downcase,
-                                           course_id: course_id,
-                                           content_id: item.content_id)
+            assignment_details = self.assignment(
+                course_id: course_id,
+                content_id: item.type == 'Assignment' ? item.content_id : item.id)
+
             item.due_dates = [{student_id: 0, due_at: assignment_details.due_at}]
 
-            overrides = self.assignment_overrides(course_id: course_id,
-                                                  assignment_id: item.content_id)
+            overrides = self.assignment_overrides(
+                course_id: course_id,
+                assignment_id: item.type == 'Assignment' ? item.content_id : item.id)
+
             unless overrides.nil?
               item.due_dates += expand_due_dates_for_students(overrides)
             end
